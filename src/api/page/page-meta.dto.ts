fa09dbf26041adaaa20efe53916663375f8b8a2c
webjsmaster <web.js.master@gmail.com>
2023-10-31 15:31:17 +0300

    feat: pagination

--- src/api/page/page-meta.dto.ts
+++ src/api/page/page-meta.dto.ts
@@ -22,8 +22,8 @@ export class PageMetaDto {
   readonly hasNextPage: boolean;
 
   constructor({ pageOptionsDto, itemCount }: PageMetaDtoParameters) {
-    this.page = pageOptionsDto.page;
-    this.take = pageOptionsDto.take;
+    this.page = +pageOptionsDto.page;
+    this.take = +pageOptionsDto.take;
     this.itemCount = itemCount;
     this.pageCount = Math.ceil(this.itemCount / this.take);
     this.hasPreviousPage = this.page > 1;
--- src/decorators/response.decorators.ts
+++ src/decorators/response.decorators.ts
@@ -1,12 +1,16 @@
-import { applyDecorators } from '@nestjs/common';
+import { Type, applyDecorators } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
+  ApiExtraModels,
   ApiForbiddenResponse,
   ApiNoContentResponse,
   ApiNotFoundResponse,
+  ApiOkResponse,
   ApiResponse,
   ApiUnauthorizedResponse,
+  getSchemaPath,
 } from '@nestjs/swagger';
+import { PageDto } from 'src/api/page/page.dto';
 
 export function ApiNotAuth() {
   return applyDecorators(
@@ -116,3 +120,27 @@ export function ApiStatusOkResponse() {
     }),
   );
 }
+
+export const ApiPaginatedResponse = <TModel extends Type<any>>(
+  model: TModel,
+) => {
+  return applyDecorators(
+    ApiExtraModels(PageDto),
+    ApiOkResponse({
+      description: "Successfully received model list",
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(PageDto) },
+          {
+            properties: {
+              data: {
+                type: "array",
+                items: { $ref: getSchemaPath(model) },
+              },
+            },
+          },
+        ],
+      },
+    }),
+  );
+};
